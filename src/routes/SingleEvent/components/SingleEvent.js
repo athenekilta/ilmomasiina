cd44611457e9ca4577dd9be6b7df9e18157c50da
Alan Pirdil <pirdil.alan@gmail.com>
2016-12-28 21:36:50 +0200

    random fixes

--- src/routes/SingleEvent/components/SingleEvent.js
+++ src/routes/SingleEvent/components/SingleEvent.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './SingleEvent.scss';
 
-/* Render a single item
-*/
+class AttendButton extends React.Component {
+  render() {
+    return(
+      <div>
+        <button className='col-md-12 btn btn-success'>{this.props.data.quotaName}</button>
+      </div>
+    )
+  }
+}
+
+class ViewProgress extends React.Component {
+  render(){
+    return(
+      <div>
+        <p className='col-md-12'>{this.props.data.quotaName}</p>
+        <div className='col-md-2'>
+          <p>{this.props.data.going}/{this.props.data.max}</p>
+        </div>
+        <div className='progress col-md-10'>
+          <div className='progress-bar' role='progressbar' aria-valuenow={this.props.data.going}
+            aria-valuemin="0" aria-valuemax={this.props.data.max}
+            style={{ width: this.props.data.going / this.props.data.max *100 + '%' }}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class AttendeeGroup extends React.Component {
-  render() {
+  render () {
     const RegistrationRow = attendee =>
-      <tr>
-        <td>{attendee.data}</td>
-        <td>{attendee.data}</td>
-        <td>{attendee.data}</td>
-      </tr>;
+    <tr>
+      <td>{attendee.data}</td>
+      <td>{attendee.data}</td>
+      <td>{attendee.data}</td>
+    </tr>
 
     return (
       <div>
@@ -25,16 +51,16 @@ class AttendeeGroup extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {
+          {
             this.props.data.attendees.map(
-              row =>
-                <RegistrationRow data={row} />)
+              (i, index) =>
+                <RegistrationRow data={i} />)
           }
           </tbody>
         </table>
-        <hr />
+        <hr/>
       </div>
-    );
+    )
   }
 }
 
@@ -47,19 +73,38 @@ class SingleEvent extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.singleEvent.title}</h1>
-        <p>
-          {this.props.singleEvent.date}<br />
-          Hinta: {this.props.singleEvent.price}<br />
-          <a href='http://pekkalammi.com'>Facebook-tapahtuma</a>
-        </p>
-        <p>{this.props.singleEvent.description}</p>
-        <h2>Ilmoittautuneet</h2>
-        {typeof this.props.singleEvent.quota !== 'undefined' ?
-        (this.props.singleEvent.quota.map(
-          (i, index) =>
-            <AttendeeGroup key={index} data={i} />)
-          ) : '' }
+        <div className='col-md-8'>
+          <h1>{this.props.singleEvent.title}</h1>
+          <p>
+            {this.props.singleEvent.date}<br />
+            Hinta: {this.props.singleEvent.price}<br />
+            <a href='http://pekkalammi.com'>Facebook-tapahtuma</a>
+          </p>
+          <p>{this.props.singleEvent.description}</p>
+          <h2>Ilmoittautuneet</h2>
+          {typeof this.props.singleEvent.quota !== 'undefined' ?
+          (this.props.singleEvent.quota.map(
+            (i, index) =>
+              <AttendeeGroup key={index} data={i} />)
+            ) : '' }
+        </div>
+        <div className='asideComponent col-md-4 pull-right'>
+          <h3 className='col-md-12'>Ilmoittaudu</h3>
+          {this.props.singleEvent.quota.map(
+            (i, index) =>
+            <AttendButton key={index} data={i} />
+          )
+
+          }
+        </div>
+        <div className='asideComponent col-md-4 pull-right'>
+          <h3 className='col-md-12'>Ilmoittautuneet</h3>
+          {this.props.singleEvent.quota.map(
+            (i, index) =>
+            <ViewProgress key={index} data={i} />
+            )
+          }
+        </div>
       </div>
     );
   }
