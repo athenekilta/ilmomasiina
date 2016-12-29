e4410b653b563f9d0ecd06a17bf1f5688292f316
Pekka Lammi <pekka.lammi@aalto.fi>
2016-12-29 14:20:21 +0200

    create quota table and hook it

--- server/services/index.js
+++ server/services/index.js
@@ -4,6 +4,8 @@ const hooks = require('feathers-hooks');
 const knex = require('knex');
 const ilmoconfig = require('../../config/ilmomasiina.config.js');
 
+// const user require('./user');
+
 module.exports = function () { // 'function' needed as we use 'this'
   debug('Feathers');
   const app = this;
@@ -20,29 +22,44 @@ module.exports = function () { // 'function' needed as we use 'this'
     },
   });
 
-  db.schema.dropTableIfExists('events').then(() => {
-    debug('Dropped events table');
-  }).then(() => db.schema.dropTableIfExists('attendees')).then(() => db.schema.createTable('events', (table) => {
-    debug('Creating events table');
-    table.increments('id');
-    table.string('name');
-    table.dateTime('date');
-    table.string('description');
-    table.string('price');
-    table.string('location');
-    table.string('homepage');
-    table.string('facebooklink');
-  }))
-  .then(() => db.schema.createTable('attendees', (table) => {
-    debug('Creating attendees table');
-    table.increments('id');
-    table.integer('eventId');
-    table.string('name');
-    table.string('email');
-  }))
+  // drop tables
+  db.schema.dropTableIfExists('events')
+  .then(() => db.schema.dropTableIfExists('attendees'))
+  .then(() => db.schema.dropTableIfExists('quotas'))
+
+  // create tables
+  .then(() =>
+    db.schema.createTable('events', (table) => {
+      debug('Creating events table');
+      table.increments('id');
+      table.string('name');
+      table.dateTime('date');
+      table.string('description');
+      table.string('price');
+      table.string('location');
+      table.string('homepage');
+      table.string('facebooklink');
+    }))
+  .then(() =>
+    db.schema.createTable('attendees', (table) => {
+      debug('Creating attendees table');
+      table.increments('id');
+      table.integer('eventId');
+      table.string('name');
+      table.string('email');
+    }))
+  .then(() =>
+    db.schema.createTable('quotas', (table) => {
+      debug('Creating quotas table');
+      table.increments('id');
+      table.integer('eventId');
+      table.string('quotaName');
+      table.integer('quotaSize');
+      table.dateTime('quotaOpens');
+      table.dateTime('quotaCloses');
+    }))
   .then(() => {
     // create dummy events
-
     app.service('/api/events').create({
       name: 'Tapahtuma1',
       date: '2017-1-1 23:59:59',
@@ -105,17 +122,41 @@ module.exports = function () { // 'function' needed as we use 'this'
     name: 'attendees',
   }));
 
+  app.use('/api/quotas', service({
+    Model: db,
+    name: 'quotas',
+  }));
+
   const schema = {
     include: [{
       service: '/api/attendees',
       nameAs: 'attendees',
       parentField: 'id',
       childField: 'eventId',
-    }]
-  }
+    },
+    {
+      service: '/api/quotas',
+      nameAs: 'quotas',
+      parentField: 'id',
+      childField: 'eventId',
+    }],
+  };
 
   // hook
   app.service('/api/events').after({
-    get: hooks.populate({schema}),
+    /* populate get-function result
+    with data from attendee and quota tables */
+    get: hooks.populate({ schema }),
+
+    create(hook) {
+      // creates a new quota and attaches it to just created event
+      app.service('/api/quotas').create({
+        eventId: hook.result.id,
+        quotaName: 'Kiintiö tapahtumalle ',
+        quotaSize: 20,
+        quotaOpens: '2017-1-1 23:59:59',
+        quotaCloses: '2017-1-1 23:59:59',
+      });
+    },
   });
 };
