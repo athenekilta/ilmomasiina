22a851150d011bb06b1bd12b5a1ed5bb1b94afd9
Ville Vuorenmaa <villevuor@gmail.com>
2016-12-29 15:37:04 +0200

    Search actions

--- src/routes/Admin/components/AdminEventList.js
+++ src/routes/Admin/components/AdminEventList.js
@@ -26,20 +26,72 @@ class AdminEventListItem extends React.Component {
 */
 
 class AdminEventList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: 0,
+      searchString: '',
+    };
+
+    this.changeYear = this.changeYear.bind(this);
+    this.changeSearchString = this.changeSearchString.bind(this);
+    this.doSearch = this.doSearch.bind(this);
+  }
+
   componentWillMount() {
     this.props.getAdminEventList();
   }
 
+  changeYear(year) {
+    const state = this.state;
+    state.year = year;
+    this.setState(state);
+    this.doSearch();
+  }
+
+  changeSearchString(str) {
+    const state = this.state;
+    state.searchString = str;
+    this.setState(state);
+    this.doSearch();
+  }
+
+  doSearch() {
+    console.log(this.state);
+  }
+
   render() {
+    const yearOptions = [];
+    for (let year = 2016; year <= new Date().getFullYear(); year += 1) {
+      yearOptions.push(<option value={year}>{year}</option>);
+    }
+
     return (
       <div>
         <Link to="/admin/edit" className="btn btn-success btn-lg pull-right">+ Uusi tapahtuma</Link>
         <h1>Hallinta</h1>
         <div className="alert alert-info" role="alert">
           <p>
-            <strong>Ville Vuorenmaa</strong> on pyytänyt pääsyä hallintapaneeliin.&nbsp;
+            <strong>Ville Vuorenmaa</strong> on pyytänyt pääsyä hallintapaneeliin.<br />
             <a>Hyväksy</a><span className="separator">/</span><a>Hylkää</a>
           </p>
+        </div>
+        <div className="search-form">
+          <select className="form-control pull-left" onChange={event => this.changeYear(event.target.value)}>
+            <option value={0}>Hae vuoden mukaan</option>
+            {yearOptions}
+          </select>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Hae tapahtumaa…"
+              onKeyPress={event => (event.key === 'Enter' ? this.changeSearchString(event.target.value) : '')}
+            />
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="button" onClick={this.doSearch}>Hae</button>
+            </span>
+          </div>
         </div>
         <table className="table">
           <thead>
