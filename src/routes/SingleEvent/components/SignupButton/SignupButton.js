f26fa715577e5b66fa2185ccb8779333521c62df
Ville Vuorenmaa <villevuor@gmail.com>
2016-12-30 16:46:25 +0200

    Logic fix

--- src/routes/SingleEvent/components/SignupButton/SignupButton.js
+++ src/routes/SingleEvent/components/SignupButton/SignupButton.js
@@ -8,14 +8,14 @@ export class SignupButton extends React.Component {
     const now = moment();
 
     const isOpened = moment(this.props.opens).isSameOrAfter(now);
-    const isClosed = moment(this.props.closes).isSameOrAfter(now);
+    const isClosed = now.isSameOrAfter(moment(this.props.closes));
 
     const state = signupState(this.props.eventTime, this.props.opens, this.props.closes);
 
     return (
       <p className="text-center">
         <button
-          disabled={(isOpened)}
+          disabled={(isOpened && !isClosed)}
           className='btn btn-success btn-block'
           onClick={() => this.props.openForm()}
         >
@@ -32,7 +32,7 @@ SignupButton.propTypes = {
   title: React.PropTypes.string.isRequired,
   opens: React.PropTypes.string.isRequired,
   closes: React.PropTypes.string.isRequired,
-  eventTime: React.PropTypes.string.isRequired,
+  eventTime: React.PropTypes.string,
 };
 
 export default SignupButton;
