3045a62d13ff85d4518648eee86f1516daab5b31
Ville Vuorenmaa <villevuor@gmail.com>
2017-01-05 17:04:49 +0200

    Conditional statements fixed

--- src/routes/SingleEvent/components/SignupButton/SignupButton.js
+++ src/routes/SingleEvent/components/SignupButton/SignupButton.js
@@ -8,16 +8,15 @@ export class SignupButton extends React.Component {
     const now = moment();
 
     const isOpened = now.isSameOrAfter(moment(this.props.opens));
-    const isClosed = moment(this.props.closes).isSameOrAfter(now);
+    const isClosed = now.isSameOrAfter(moment(this.props.closes));
+    const isOpen = (isOpened && !isClosed);
 
     const state = signupState(this.props.eventTime, this.props.opens, this.props.closes);
 
-    const isOpen = (isOpened && !isClosed);
-
     return (
-      <p className="text-center">
+      <p>
         <button
-          disabled={isOpen}
+          disabled={!isOpen}
           className="btn btn-success btn-block"
           onClick={() => (isOpen ? this.props.openForm() : {})}
         >
