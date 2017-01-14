33cb8e6ebf4c7bf542828869834e9aa13654c477
Ville Vuorenmaa <villevuor@gmail.com>
2017-01-14 02:22:46 +0200

    Proper data to event endpoint

--- server/services/event/hooks.js
+++ server/services/event/hooks.js
@@ -1,23 +1,69 @@
-const includeAssociatedModels = (hook) => {
-  if (hook.params.query.$select) return; // if selecting specific columns, do not include
-
+const includeQuotas = (hook) => {
   const sequelize = hook.app.get('sequelize');
 
   hook.params.sequelize = {
-    distinct: true, // must set this in order to get correct total count
+    attributes: ['id', 'title', 'date'],
+    distinct: true,
+    // Include quotas of event and count of signups
+    include: [{
+      model: sequelize.models.quota,
+      attributes: [
+        'title',
+        'size',
+        'signupOpens',
+        'signupCloses',
+        [sequelize.fn('COUNT', sequelize.col('quota.signups.id')), 'signupCount'],
+      ],
+      include: [{
+        model: sequelize.models.signup,
+        attributes: [],
+      }],
+    }],
+    group: [sequelize.col('event.id'), sequelize.col('quota.id')],
+  };
+};
+
+
+const includeAllEventData = (hook) => {
+  const sequelize = hook.app.get('sequelize');
+
+  hook.params.sequelize = {
+    distinct: true,
     include: [
+      // First include all questions (also non-public for the form)
       {
+        attributes: ['id', 'question', 'type', 'options', 'required', 'public'],
+        model: sequelize.models.question,
+      },
+      // Include quotas..
+      {
+        attributes: ['title', 'size', 'signupOpens', 'signupCloses'],
         model: sequelize.models.quota,
+        // ... and signups of quotas
+        include: [{
+          attributes: ['firstName', 'lastName'],
+          model: sequelize.models.signup,
+          // ... and answers of signups
+          include: [{
+            attributes: ['questionId', 'answer'],
+            model: sequelize.models.answer,
+            // ... but only public ones
+            include: [{
+              model: sequelize.models.question,
+              attributes: [],
+              where: { public: true },
+            }],
+          }],
+        }],
       },
     ],
-    // exclude: 'subCategories'
   };
 };
 
 exports.before = {
   all: [],
-  find: [includeAssociatedModels],
-  get: [includeAssociatedModels],
+  find: [includeQuotas],
+  get: [includeAllEventData],
   create: [],
   update: [],
   patch: [],
