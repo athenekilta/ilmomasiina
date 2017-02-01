6604c1377d13982ee6d165b113968d5799cf4af7
Alan Pirdil <pirdil.alan@gmail.com>
2017-02-01 16:56:32 +0200

    404 text

--- src/routes/404/components/FourZeroFour.js
+++ src/routes/404/components/FourZeroFour.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './FourZeroFour.scss';
 
-class FourZeroFour extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>404!</h1>
-        <p>Something went wrong</p>
-      </div>
-    );
-  }
-}
+const FourZeroFour = () => (
+  <div className="container">
+    <div className="FourZeroFour col-xs-12">
+      <h1>404. Page not found.</h1>
+      <p>We're terribly sorry but at this given moment the website you're looking for cannot be reached or found.</p>
+    </div>
+
+  </div>
+);
 
 export default FourZeroFour;
