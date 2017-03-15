924e2098af7bb415d6b55bafcba1734184b8ac54
Ville Vuorenmaa <villevuor@gmail.com>
2017-03-15 12:12:34 +0200

    Removed unnecessary empty values

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -7,8 +7,8 @@ const Login = () => (
     <div className="col-xs-12 col-sm-8 col-md-6">
       <h1>Kirjaudu sisään</h1>
       <Formsy.Form>
-        <Input value="" label="Sähköposti" name="email" title="Email" required />
-        <Input value="" label="Salasana" name="password" title="Password" type="password" required />
+        <Input label="Sähköposti" name="email" title="Sähköposti" required />
+        <Input label="Salasana" name="password" title="Salasana" type="password" required />
         <p><button className="btn btn-primary btn-lg" type="submit">Kirjaudu</button></p>
         <p><a>Unohditko salasanasi?</a></p>
       </Formsy.Form>
