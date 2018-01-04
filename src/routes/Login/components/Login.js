a264733cd3054622f717fce841593c8c06fbd477
Pekka Lammi <pekka.lammi@aalto.fi>
2018-01-04 15:21:41 +0200

    first iteration of /login

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import Formsy from 'formsy-react';
 import { Input } from 'formsy-react-components';
+import { connect } from 'react-redux';
+
 import './Login.scss';
+import { loginUser } from '../modules/login';
 
-const Login = () => (
-  <div className="container">
-    <h1>Login</h1>
-    <div className="Login col-xs-8 col-md-6 align-self-center">
-      <Formsy.Form className="credentials">
-        <Input value="" label="Sähköposti" name="email" title="Email" required />
-        <Input value="" label="Salasana" name="password" title="Password" type="password" required />
-        <div className="nuppi">
-          <button className="nappi btn-success col-xs-3" type="submit">Kirjaudu</button>
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Login</h1>
+        { this.props.loginError
+          ? <div> {this.props.loginError.body} </div>
+          : ''
+        }
+        <div className="Login col-xs-8 col-md-6 align-self-center">
+          <Formsy.Form className="credentials">
+            <Input id="email" value="" label="Sähköposti" name="email" title="Email" required />
+            <Input id="password" value="" label="Salasana" name="password" title="Password" type="password" required />
+            <div className="nuppi">
+              <button
+                className="nappi btn-success col-xs-3"
+                onClick={() => this.props.loginUser({
+                  username: document.getElementById('email').value,
+                  password: document.getElementById('password').value,
+                })}
+              >
+                Kirjaudu
+              </button>
+            </div>
+          </Formsy.Form>
         </div>
-      </Formsy.Form>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
-export default Login;
+const mapStateToProps = state => ({
+  loginError: state.login.errorMessage,
+});
+
+const mapDispatchToProps = {
+  loginUser,
+};
+
+Login.propTypes = {
+  loginError: React.PropTypes.string,
+  loginUser: React.PropTypes.func,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
