60b1354d12b83f7d78ce59e5f7289ec3d7adc592
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-09-04 11:55:34 +0300

    remove config from gitignore

---
+++ config/ilmomasiina.config.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+
+const config = {
+  mysqlUser: process.env.MYSQL_USER,
+  mysqlPassword: process.env.MYSQL_PASSWORD,
+  mysqlDatabase: process.env.MYSQL_DATABASE,
+  editTokenSalt: process.env.EDIT_TOKEN_SALT,
+  mailFrom: process.env.MAIL_FROM,
+  feathersAuthSecret: process.env.FEATHERS_AUTH_SECRET,
+  sendgridApiKey: process.env.SENDGRID_API_KEY,
+  baseUrl: process.env.BASE_URL,
+};
+
+_.forOwn(config, (value, key) => {
+  if (!value) {
+    console.error(`Missing .env variable: ${key}, please check /config/ilmomasiina.config.js`);
+  }
+});
+
+module.exports = config;
