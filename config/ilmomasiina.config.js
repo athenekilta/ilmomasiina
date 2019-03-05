c47aea90e600fb133575b51633d5a58f216f3a0e
jonesus <joonas.palosuo@gmail.com>
2019-03-05 20:05:33 +0200

    Fix missing default

--- config/ilmomasiina.config.js
+++ config/ilmomasiina.config.js
@@ -9,7 +9,7 @@ const config = {
   feathersAuthSecret: process.env.FEATHERS_AUTH_SECRET,
   sendgridApiKey: process.env.SENDGRID_API_KEY,
   baseUrl: process.env.BASE_URL,
-  prefixUrl: process.env.PREFIX_URL,
+  prefixUrl: process.env.PREFIX_URL || '',
   adminRegistrationAllowed: process.env.ADMIN_REGISTRATION_ALLOWED == "true" || false,
   brandingMailFooterText: process.env.BRANDING_MAIL_FOOTER_TEXT,
   brandingMailFooterLink: `${process.env.BASE_URL}${process.env.PREFIX_URL || ''}`,
