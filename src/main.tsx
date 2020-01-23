0f51d21eb8bd6bb738a2219cdad292931b148ab2
rriski <timo.h.riski@outlook.com>
2020-01-23 23:02:17 +0200

    Add whyDidYouRender
    Used to debug why components rerender. Can be used to do performance optimization.
    See https://github.com/welldone-software/why-did-you-render.

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,11 @@ import AppContainer from './containers/AppContainer';
 
 import 'react-dates/lib/css/_datepicker.css';
 
+if (process.env.NODE_ENV !== 'production') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React);
+}
+
 if (PROD) {
   Sentry.init({ dsn: sentryDsn });
 }
