5ceb9824d31161e6e6b12e17fff5bf757a31f300
Petrus Asikainen <petrus.asikainen@aalto.fi>
2021-06-16 03:10:35 +0300

    Remove dependency from dev-server.ts to project.config.js

--- server/bin/dev-server.ts
+++ server/bin/dev-server.ts
@@ -1,10 +1,9 @@
 import debug from 'debug';
 import app from '../app';
 
-const project = require('../../../config/project.config');
-
-const server = app.listen(project.server_port);
+const port = process.env.PORT || 3000;
+const server = app.listen(port);
 
 const debugLog = debug('app:bin:dev-server');
 
-server.on('listening', () => debugLog(`Server is now running at http://localhost:${project.server_port}.`));
+server.on('listening', () => debugLog(`Server is now running at http://localhost:${port}.`));
