14fbb85e497c7334dd516b82ca937f23caaaba14
Visa Pollari <visa.pollari@gmail.com>
2021-06-20 17:48:12 +0300

    handle missing prefix_url

--- config/environments.config.js
+++ config/environments.config.js
@@ -8,7 +8,7 @@ module.exports = {
   // NOTE: In development, we use an explicit public path when the assets
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
-  development: config => ({
+  development: (config) => ({
     compiler_public_path: `http://${config.server_host}:${config.server_port}/`,
   }),
 
@@ -16,7 +16,7 @@ module.exports = {
   // Overrides when NODE_ENV === 'production'
   // ======================================================
   production: () => ({
-    compiler_public_path: `${process.env.PREFIX_URL}/`,
+    compiler_public_path: `${process.env.PREFIX_URL || ''}/`,
     compiler_fail_on_warning: false,
     compiler_hash_type: 'chunkhash',
     compiler_devtool: null,
