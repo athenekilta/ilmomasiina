16fc6e79b90945a3c476e09f9994ed7394537aeb
Petrus Asikainen <petrus.asikainen@aalto.fi>
2021-09-29 02:24:02 +0300

    Clean up computeSignupPosition

--- server/services/signup/computeSignupPosition.ts
+++ server/services/signup/computeSignupPosition.ts
@@ -6,11 +6,6 @@ import { Event } from '../../models/event';
 import { Quota } from '../../models/quota';
 import { Signup, SignupStatus } from '../../models/signup';
 
-export type SignupPositionStatus = {
-  status: SignupStatus;
-  position: number;
-};
-
 async function sendPromotedFromQueueEmail(signup: Signup, eventId: Event['id']) {
   if (signup.email === null) return;
 
@@ -31,82 +26,93 @@ async function sendPromotedFromQueueEmail(signup: Signup, eventId: Event['id']) 
  *
  * Make sure that the event passed contains `id`, `openQuotaSize`.
  */
-export function refreshSignupPositions(event: Event): Promise<Map<Signup['id'], SignupPositionStatus>> {
-  return Event.sequelize!.transaction(async (transaction) => {
-    const signups = await Signup.findAll({
-      attributes: ['id', 'quotaId', 'email', 'status', 'position'],
-      include: [
-        {
-          model: Quota,
-          attributes: ['id', 'size'],
-        },
-      ],
-      where: {
-        '$quota.eventId$': event.id,
-      } as WhereOptions,
-      // Honor creation time, tie-break by random ID in case of same millisecond
-      order: [
-        ['createdAt', 'ASC'],
-        ['id', 'ASC'],
-      ],
-      // Prevent simultaneous changes
-      lock: Transaction.LOCK.UPDATE,
-      transaction,
-    });
+export async function refreshSignupPositions(event: Event, transaction?: Transaction): Promise<Signup[]> {
+  // Wrap in transaction if not given
+  if (!transaction) {
+    return Event.sequelize!.transaction(
+      async (trans) => refreshSignupPositions(event, trans),
+    );
+  }
 
-    const quotaSignups = new Map<Quota['id'], number>();
-    let inOpenQuota = 0;
-    let inQueue = 0;
-
-    const result = await Promise.all(signups.map(async (signup: Signup) => {
-      let status: SignupStatus;
-      let position: number;
-
-      let inChosenQuota = quotaSignups.get(signup.quotaId) ?? 0;
-      const chosenQuotaSize = signup.quota!.size ?? Infinity;
-
-      // Assign the selected or open quotas if free. Never worsen a signup's status.
-      if (signup.status === 'in-quota' || inChosenQuota < chosenQuotaSize) {
-        inChosenQuota += 1;
-        quotaSignups.set(signup.quotaId, inChosenQuota);
-        status = 'in-quota';
-        position = inChosenQuota;
-      } else if (signup.status === 'in-open' || inOpenQuota < event.openQuotaSize) {
-        inOpenQuota += 1;
-        status = 'in-open';
-        position = inOpenQuota;
-      } else {
-        inQueue += 1;
-        status = 'in-queue';
-        position = inQueue;
-      }
-
-      // If the signup was just promoted from the queue, send an email about it asynchronously.
-      if (signup.status === 'in-queue' && status !== 'in-queue') {
-        sendPromotedFromQueueEmail(signup, event.id);
-      }
-
-      // Store changes in database, if any
-      if (signup.status !== status || signup.position !== position) {
-        await signup.update(
-          { status, position },
-          { transaction },
-        );
-      }
-
-      return <const>[signup.id, { status, position }];
-    }));
-
-    return new Map(result);
+  const signups = await Signup.findAll({
+    attributes: ['id', 'quotaId', 'email', 'status', 'position'],
+    include: [
+      {
+        model: Quota,
+        attributes: ['id', 'size'],
+      },
+    ],
+    where: {
+      '$quota.eventId$': event.id,
+    } as WhereOptions,
+    // Honor creation time, tie-break by random ID in case of same millisecond
+    order: [
+      ['createdAt', 'ASC'],
+      ['id', 'ASC'],
+    ],
+    // Prevent simultaneous changes
+    lock: Transaction.LOCK.UPDATE,
+    transaction,
   });
+
+  // Assign each signup to a quota or the queue.
+  const quotaSignups = new Map<Quota['id'], number>();
+  let inOpenQuota = 0;
+  let inQueue = 0;
+
+  const result = signups.map((signup: Signup) => {
+    let status: SignupStatus;
+    let position: number;
+
+    let inChosenQuota = quotaSignups.get(signup.quotaId) ?? 0;
+    const chosenQuotaSize = signup.quota!.size ?? Infinity;
+
+    // Assign the selected or open quotas if free. Never worsen a signup's status.
+    if (signup.status === 'in-quota' || inChosenQuota < chosenQuotaSize) {
+      inChosenQuota += 1;
+      quotaSignups.set(signup.quotaId, inChosenQuota);
+      status = 'in-quota';
+      position = inChosenQuota;
+    } else if (signup.status === 'in-open' || inOpenQuota < event.openQuotaSize) {
+      inOpenQuota += 1;
+      status = 'in-open';
+      position = inOpenQuota;
+    } else {
+      inQueue += 1;
+      status = 'in-queue';
+      position = inQueue;
+    }
+
+    return { signup, status, position };
+  });
+
+  // If a signup was just promoted from the queue, send an email about it asynchronously.
+  result.forEach(({ signup, status }) => {
+    if (signup.status === 'in-queue' && status !== 'in-queue') {
+      sendPromotedFromQueueEmail(signup, event.id);
+    }
+  });
+
+  // Store changes in database, if any.
+  await Promise.all(result.map(async ({ signup, status, position }) => {
+    if (signup.status !== status || signup.position !== position) {
+      await signup.update(
+        { status, position },
+        { transaction },
+      );
+    }
+  }));
+
+  return result.map(({ signup }) => signup);
 }
 
 /**
  * Like `refreshSignupPositions`, but returns the status for the given signup.
  */
 export async function refreshSignupPositionsAndGet(event: Event, signupId: Signup['id']) {
-  const positionMap = await refreshSignupPositions(event);
-  const position = positionMap.get(signupId);
-  if (!position) throw new Error('failed to compute status');
-  return position;
+  const result = await refreshSignupPositions(event);
+  const signup = result.find(({ id }) => id === signupId);
+  if (!signup) throw new Error('failed to compute status');
+  const { status, position } = signup;
+  return { status, position };
 }
