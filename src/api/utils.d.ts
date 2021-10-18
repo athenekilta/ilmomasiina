ea485979568565f9408d84667d55ac93dd18ad52
Petrus Asikainen <petrus.asikainen@aalto.fi>
2021-10-18 18:52:01 +0300

    Add comments to StringifyAPI

--- src/api/utils.d.ts
+++ src/api/utils.d.ts
@@ -1,9 +1,15 @@
 /** Utility type that converts fields in the imported API to string. */
 export type StringifyApi<T> = {
   [P in keyof T]: (
+    // all arrays are stringified recursively
     T[P] extends (infer E)[] ? StringifyApi<E>[]
+      // Date | null --> string | null
+      // Date --> string
+      // (also matches "x: null", but that's an useless type anyway)
       : T[P] extends Date | null ? (null extends T[P] ? string | null : string)
+        // basic types: any subset of boolean | number | string | null --> itself
         : T[P] extends boolean | number | string | null ? T[P]
+          // other types (essentially, objects) are stringified recursively
           : StringifyApi<T[P]>
   );
 };
