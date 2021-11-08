30e58ec9cd208da687fabf2ebbc468474ef2abac
Petrus Asikainen <petrus.asikainen@aalto.fi>
2021-11-08 22:11:33 +0200

    Remove unused react-dates dep, left behind in earlier development

--- packages/ilmomasiina-frontend/src/index.tsx
+++ packages/ilmomasiina-frontend/src/index.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import * as Sentry from '@sentry/browser';
 import ReactDOM from 'react-dom';
 
-import 'react-dates/initialize';
-
 import AppContainer from './containers/AppContainer';
-
-import 'react-dates/lib/css/_datepicker.css';
 
 if (!PROD) {
   // eslint-disable-next-line global-require
