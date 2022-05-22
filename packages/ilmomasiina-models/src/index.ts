8d6f192da77505efc4f92dff4522446678efba63
Petrus Asikainen <petrus.asikainen@aalto.fi>
2022-05-23 00:27:34 +0300

    Publish JS packages for version tags

---
+++ packages/ilmomasiina-models/src/index.ts
@@ -0,0 +1,7 @@
+export type { AdminCategory } from './services/admin/categories';
+export type { AdminEvent } from './services/admin/events';
+export type { AdminSlug } from './services/admin/slug';
+export type { Auth } from './services/auth';
+export type { Event } from './services/events';
+export type { Signup } from './services/signups';
+export type { User } from './services/users';
