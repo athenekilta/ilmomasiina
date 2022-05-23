42a45673594a2f9ca8c2a736b274368f55e997c6
Petrus Asikainen <petrus.asikainen@aalto.fi>
2022-05-23 12:07:31 +0300

    Add migration for endDate

--- packages/ilmomasiina-backend/src/models/migrations/index.ts
+++ packages/ilmomasiina-backend/src/models/migrations/index.ts
@@ -3,10 +3,12 @@ import { RunnableMigration } from 'umzug';
 
 import _0000_initial from './0000-initial';
 import _0001_add_audit_logs from './0001-add-audit-logs';
+import _0002_add_event_endDate from './0002-add-event-endDate';
 
 const migrations: RunnableMigration<Sequelize>[] = [
   _0000_initial,
   _0001_add_audit_logs,
+  _0002_add_event_endDate,
 ];
 
 export default migrations;
