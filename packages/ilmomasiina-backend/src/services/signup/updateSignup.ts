04a3fae026a733a5290fef1f431843792e0223f5
Petrus Asikainen <petrus.asikainen@aalto.fi>
2022-07-31 17:46:01 +0300

    Linter fix

--- packages/ilmomasiina-backend/src/services/signup/updateSignup.ts
+++ packages/ilmomasiina-backend/src/services/signup/updateSignup.ts
@@ -8,7 +8,6 @@ import sendSignupConfirmationEmail from '../../mail/signupConfirmation';
 import { Answer } from '../../models/answer';
 import { Event } from '../../models/event';
 import { Question } from '../../models/question';
-import { generateRandomId } from '../../models/randomId';
 import { Signup } from '../../models/signup';
 import { logEvent } from '../../util/auditLog';
 import { signupsAllowed } from './createNewSignup';
